fbed1790270563260be42c8c8dc81624cffa2180
gurpreet_singh1 <gurpreet_singh@apptunix.com>
2023-07-11 01:38:07 +0530

    design layout added

---
+++ e-commerce/src/App.js
@@ -0,0 +1,47 @@
+
+import './categories.style.scss';
+
+const App = ()=> {
+  const categories =[
+    {
+        id:1,
+        title:"Hats",
+    },
+    {
+      id:2,
+      title:"Jackets",
+    },
+    {
+      id:3,
+      title:"Sneankers",
+    },
+    {
+      id:4,
+      title:"Womens",
+    },
+    {
+      id:5,
+      title:"Mens",
+    }
+  ]
+  return (
+    <div className="categories-container">
+      {
+        categories.map((category) => (
+          <div className="category-element">
+          {/* <figure><img /></figure> */}
+          <div className="background-image" />
+             <div class="category-body">
+                 <h2>{category.title}</h2>
+                 <p>Shop Now</p>
+             </div>
+          </div>
+        ))
+      }
+    
+    
+    </div>
+  );
+}
+
+export default App;
