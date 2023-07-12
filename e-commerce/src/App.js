22bb7b4ec06d1f4ecd58f987b8dfb4f25d52928e
gurpreet_singh1 <gurpreet_singh@apptunix.com>
2023-07-13 00:29:26 +0530

    app.js code shift to new component (directory.js)

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,46 +1,41 @@
+import Directory from './components/directory/directory-component'
 
 import './categories.style.scss';
 
 const App = ()=> {
   const categories =[
-    {
-        id:1,
-        title:"Hats",
-    },
-    {
-      id:2,
-      title:"Jackets",
-    },
-    {
-      id:3,
-      title:"Sneankers",
-    },
-    {
-      id:4,
-      title:"Womens",
-    },
-    {
-      id:5,
-      title:"Mens",
-    }
-  ]
-  return (
-    <div className="categories-container">
       {
-        categories.map((category) => (
-          <div className="category-element">
-          {/* <figure><img /></figure> */}
-          <div className="background-image" />
-             <div class="category-body">
-                 <h2>{category.title}</h2>
-                 <p>Shop Now</p>
-             </div>
-          </div>
-        ))
+        id: 1,
+        title: "hats",
+        imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+      },
+      {
+        id: 2,
+        title: "jackets",
+        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+      },
+      {
+        id: 3,
+        title: "sneakers",
+        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+      },
+      {
+        id: 4,
+        title: "womens",
+        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
+      },
+      {
+        id: 5,
+        title: "mens",
+        imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
       }
+    ];
     
-    
-    </div>
+
+  return (
+    <>
+    <Directory  categories={categories}/>
+    </>
   );
 }
 
